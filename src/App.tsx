131e5f60904eed547ffbb8df99237cb4b2d6e3b5
pedrokpdo <pedromotoehm@gmail.com>
2022-05-12 14:23:34 -0300

    movimentando character

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <C.Container>
       <C.Map>
-        <Character/>
+        <Character x={6} y={4}/>
       </C.Map>
     </C.Container>
   )
--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,9 +1,20 @@
 import * as C from './styles'
 
-export const Character = () => {
+type Props = {
+    x: number;
+    y: number;
+}
+
+export const Character = ({ x, y }: Props) => {
+    const size = 30
+
     return (
-        <C.Container>
-            
+        <C.Container
+            left={x * size}
+            top={y * size}
+            size={size}
+        >
+
         </C.Container>
     )
 }
