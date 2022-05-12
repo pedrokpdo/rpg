f04339b21dfb06835ca99a4198bf2e8e214815da
pedrokpdo <pedromotoehm@gmail.com>
2022-05-12 15:18:58 -0300

    mudando lado do personagem ao mover

--- src/hooks/useCaracter.ts
+++ src/hooks/useCaracter.ts
@@ -1,31 +1,39 @@
 import { useState } from "react"
+import { CharacterSides } from '../types/CharacterSides'
 
 export const useCaracter = () => {
 
     const [pos, setPos] = useState({ x: 3, y: 5 })
+    const [side, setSide] = useState<CharacterSides>('down')
+
+
     const moveLeft = () => {
         setPos(pos => ({
             x: pos.x -1,
             y: pos.y
         }))
+        setSide('left')
     }
     const moveRight = () => {
         setPos(pos => ({
             x: pos.x +1,
             y: pos.y
         }))
+        setSide('right')
     }
     const moveDown = () => {
         setPos(pos => ({
             x: pos.x ,
             y: pos.y + 1
         }))
+        setSide('down')
     }
     const moveUp = () => {
         setPos(pos => ({
             x: pos.x ,
             y: pos.y - 1
         }))
+        setSide('up')
     }
 
     return {
@@ -34,6 +42,7 @@ export const useCaracter = () => {
         moveLeft,
         moveDown,
         moveRight,
-        moveUp
+        moveUp,
+        side
     }
 }
