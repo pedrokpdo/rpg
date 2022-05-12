cf3fc74f713e6b452f2b9dd2b59d6b343b64e4cc
pedrokpdo <pedromotoehm@gmail.com>
2022-05-12 14:56:36 -0300

    movimentando character

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
+import { useEffect } from 'react'
 import * as C from './App.styles'
 import { Character } from './components/Character'
 import { useCaracter } from './hooks/useCaracter'
 
+
+
 const App = () => {
   const char = useCaracter()
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+  }, [])
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.code) {
+      case 'KeyA':
+        char.moveLeft()
+
+        break;
+
+      case 'KeyW':
+        char.moveUp()
+
+        break;
+
+      case 'KeyD':
+        char.moveRight()
+
+        break;
+
+      case 'KeyS':
+        char.moveDown()
+
+        break
+    }
+
+  }
 
   return (
     <C.Container>
       <C.Map>
-        <Character x={6} y={4}/>
+        <Character x={char.x} y={char.y} />
       </C.Map>
     </C.Container>
   )
